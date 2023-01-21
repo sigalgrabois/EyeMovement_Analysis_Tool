d2fac381f67c48eedb52c6b25c7dbd68852c95f7
sigalgrabois <sigal.graboys2030@gmail.com>
2023-01-21 20:52:03 +0200

    add MATLAB script to extract the EDF files - create mat file from edf and then extract into csv file holds the data table with all the info

--- main.py
+++ main.py
@@ -5,18 +5,18 @@
 import pyedflib
 import numpy as np
 
+
 def upload_file(path):
     file_name = pyedflib.data.get_generator_filename()
     f = pyedflib.EdfReader(file_name)
     n = f.signals_in_file
-    signal_labels = f.getSignalLabels()
+    print(f.getLabel())
     sigbufs = np.zeros((n, f.getNSamples()[0]))
     for i in np.arange(n):
         sigbufs[i, :] = f.readSignal(i)
 
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    upload_file('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    upload_file('data\SHACHAR1-4dots.edf')
