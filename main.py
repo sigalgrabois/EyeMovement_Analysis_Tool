13828b6aa0fd3a3063ce4152b1894056ce8c274a
Shachar-Oron <shachar805@gmail.com>
2023-01-25 13:08:36 +0200

    updates on the image sizes and exploring the data

--- main.py
+++ main.py
@@ -7,24 +7,22 @@ import numpy as np
 import pandas as pd
 import tkinter as tk
 from tkinter import PhotoImage
+from tkinter import *
+from PIL import Image, ImageTk
 
 
-def upload_file(path):
-    file_name = pyedflib.data.get_generator_filename()
-    f = pyedflib.EdfReader(file_name)
-    n = f.signals_in_file
-    print(f.getLabel())
-    sigbufs = np.zeros((n, f.getNSamples()[0]))
-    for i in np.arange(n):
-        sigbufs[i, :] = f.readSignal(i)
+def close(ws):
+    ws.destroy()
 
 
+def resize_func(canvas, image, width, height):
+    resize_img = image.resize((width, width))
+    img = ImageTk.PhotoImage(resize_img)
+    canvas.config(image=img)
+    canvas.image = img
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    # connect to Matlab and run the script that will create csv file of the data
-    # open the csv file and read it into a pandas data frame
-    df = pd.read_csv('data/Data.csv', header=0, delimiter=",")
+
+def show_image(df):
     # make a list out of the 3th column in the data frame - start x of the eye movement
     # Extract the desired columns
     # save the values from the 3th column in a list not with loc
@@ -32,34 +30,49 @@ if __name__ == '__main__':
     start_y = df['y start (pixels)'].values.tolist()
     end_x = df['x end (pixels)'].values.tolist()
     end_y = df['y end (pixels)'].values.tolist()
-    root = tk.Tk()
-    root.title("Image Viewer")
 
-    # create a canvas to hold the image
-    canvas = tk.Canvas(root, width=600, height=400)
-    canvas.pack()
+    start_diffx = df['x start (pixels)'].values.tolist()
+    start_diffy = df['y start (pixels)'].values.tolist()
+    end_diffx = df['x end (pixels)'].values.tolist()
+    end_diffy = df['y end (pixels)'].values.tolist()
+
+    root = tk.Tk()
+    root.title("eyes movement")
+
 
     # open the image file and convert it to a PhotoImage object
+    image = PhotoImage(file="data/image.png").zoom(7)
+    image_width, image_height = image.width(), image.height()
+    # create a canvas to hold the image
+    canvas = tk.Canvas(root, width=1500, height=1100)
+    canvas_width, canvas_height = canvas.winfo_width(), canvas.winfo_height()
 
-    image = PhotoImage(file="data/image.png").zoom(2)
+
+    canvas.pack()
 
     # display the image on the canvas
     canvas.create_image(0, 0, anchor='nw', image=image)
-
     # make a zip of the two lists - start x and start y
     start = list(zip(start_x, start_y))
     end = list(zip(end_x, end_y))
-    # drow the points on the image
+    # draw the points on the image
     for i in range(len(start)):
         x1, y1 = start[i]
         x2, y2 = end[i]
         canvas.create_oval(x1, y1, x2, y2, fill='red', outline='red')
 
+    # button = tk.Button(root,
+    #                    text='EXIT PROGRAM',
+    #                    font=("Times", 14),
+    #                    command=lambda: close(root)
+    #                    )
+    # button.pack(pady=100)
     root.mainloop()
 
 
-
-
-
-
-
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # connect to Matlab and run the script that will create csv file of the data
+    # open the csv file and read it into a pandas data frame
+    df = pd.read_csv('data/Data.csv', header=0, delimiter=",")
+    show_image(df)
